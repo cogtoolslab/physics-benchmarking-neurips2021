fee7a760bc070ffe48c412e60033a80d20d692aa
Felix Binder <me@felixbinder.net>
2021-05-17 17:57:36 +0000

    yet another dominoes port change

--- experiments/dominoes_pilot/app.js
+++ experiments/dominoes_pilot/app.js
@@ -77,7 +77,7 @@ function omit(obj, props) { //helper function to remove _id of stim object
 function initializeWithTrials(socket) {
   var gameid = UUID();
   var colname = 'human-physics-benchmarking-dominoes-pilot_production_1'; //insert STIMULI DATASETNAME here
-  sendPostRequest('http://localhost:8044/db/getstims', {
+  sendPostRequest('http://localhost:8018/db/getstims', {
     json: {
       dbname: 'stimuli',
       colname: colname,
@@ -115,7 +115,7 @@ var UUID = function() {
 
 var writeDataToMongo = function(data) {
   sendPostRequest(
-    'http://localhost:8044/db/insert',
+    'http://localhost:8018/db/insert',
     { json: data },
     (error, res, body) => {
       if (!error && res.statusCode === 200) {
