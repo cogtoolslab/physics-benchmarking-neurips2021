bf168653ee25fe97b3a4e02602c143785751f32f
Felix Binder <me@felixbinder.net>
2021-05-26 19:49:55 +0000

    Updated clothiness iteration

--- experiments/clothiness_pilot/app.js
+++ experiments/clothiness_pilot/app.js
@@ -76,7 +76,7 @@ function omit(obj, props) { //helper function to remove _id of stim object
 
 function initializeWithTrials(socket) {
   var gameid = UUID();
-  var colname = 'human-physics-benchmarking-clothiness-pilot_production_1'; //insert STIMULI DATASETNAME here
+  var colname = 'human-physics-benchmarking-clothiness-pilot_production_2'; //insert STIMULI DATASETNAME here
   sendPostRequest('http://localhost:8020/db/getstims', {
     json: {
       dbname: 'stimuli',
