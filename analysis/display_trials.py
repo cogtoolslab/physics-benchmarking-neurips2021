37263248d7da6a042d482077b3ad2b625ca29394
dylantao <dylantaosirui@outlook.com>
2021-06-28 10:43:20 -0700

    complete basic analysis for human experiment trial analysis

--- analysis/display_trials.py
+++ analysis/display_trials.py
@@ -2,19 +2,24 @@ import os
 from ipywidgets import Output, GridspecLayout
 from IPython import display
 
-def plot_by_3(Dacc, video_dir):
-    trials_33 = Dacc[Dacc["correct"]<=0.33] # subset of the dominoes trials with 33.3% or lower accuracy
-    trail_accu = trials_33["correct"].tolist() # corresponding accuracy for the trials
-    trail_ans = trials_33["answer"].tolist() # corresponding ground truth for the trials
-    video_ids = trials_33.index.tolist() # corresponding stim_IDs for the trials
+# range is from 0 (min) to 1 (max)
+# num is how many videos to display in a row
+def plot_by_n(Dacc, video_dir, min, max, num=3):
+    if max < 1:
+        trials = Dacc[(Dacc["correct"]>=min) & (Dacc["correct"]<max)] # subset of the dominoes trials with corresponding accuracy
+    else:
+        trials = Dacc[(Dacc["correct"]>=min) & (Dacc["correct"]<=max)] # equal to max if it is 1
+    trail_accu = trials["correct"].tolist() # corresponding accuracy for the trials
+    trail_ans = trials["answer"].tolist() # corresponding ground truth for the trials
+    video_ids = trials.index.tolist() # corresponding stim_IDs for the trials
     video_names = [i + '.mp4' for i in video_ids]
     video_paths = [os.path.join(video_dir,i) for i in video_names]
 
     # display videos with low accuracy 
-    if (len(video_paths)%3 != 0):
-        grid = GridspecLayout(2 * (len(video_paths)//3 + 1), 3)
+    if (len(video_paths)%num != 0):
+        grid = GridspecLayout(2 * (len(video_paths)//num + 1), num)
     else:
-        grid = GridspecLayout(2 * (len(video_paths)//3), 3)
+        grid = GridspecLayout(2 * (len(video_paths)//num), num)
 
     for i, filepath in enumerate(video_paths):
         # display videos
@@ -30,9 +35,9 @@ def plot_by_3(Dacc, video_dir):
         with out_t:
             display.display(display.Markdown(data = content))
 
-        row_index = (i // 3) * 2
+        row_index = (i // num) * 2
         row_index_t = row_index + 1
-        column_index = (i % 3)
+        column_index = (i % num)
         column_index_t = column_index
         grid[row_index, column_index] = out
         grid[row_index_t, column_index_t] = out_t
