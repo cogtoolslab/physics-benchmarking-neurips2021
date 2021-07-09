63afb4b112ad29ed6845d8d90b2aad9f0ca6c3dc
Elias Wang <ewang921@gmail.com>
2021-07-09 16:02:32 -0700

    fix script args

--- stimuli/generation/drape/generate_drape.sh
+++ stimuli/generation/drape/generate_drape.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 scenario=drape
-controller_dir=${3-$HOME"/tdw_physics/tdw_physics/target_controllers/"}
-output_dir=${4-$HOME"/physion_data/"}
+output_dir=${1-$HOME"/physion_data"}
+controller_dir=${2-$HOME"/tdw_physics"}
 
 height=256
 width=256
@@ -16,7 +16,7 @@ do
     case $arg_name in
         (./*familiarization*) continue;;
     esac
-    cmd="python3 "$controller_dir$scenario".py @$arg_name""commandline_args.txt --dir "$output_dir$scenario"${arg_name#.}"$group" --height "$height" --width "$width" --seed "$seed" --save_passes '' --write_passes '_img,_id' --save_meshes --num_multiplier "$multiplier" --training_data_mode"
+    cmd="python3 "$controller_dir"/tdw_physics/target_controllers/"$scenario".py @$arg_name""commandline_args.txt --dir "$output_dir"/"$scenario"${arg_name#.}"$group" --height "$height" --width "$width" --seed "$seed" --save_passes '' --write_passes '_img,_id' --save_meshes --num_multiplier "$multiplier" --training_data_mode"
     echo $cmd
     eval " $cmd"
 done
@@ -30,7 +30,7 @@ do
     case $arg_name in
         (./*familiarization*) continue;;
     esac
-    cmd="python3 "$controller_dir$scenario".py @$arg_name""commandline_args.txt --dir "$output_dir$scenario"${arg_name#.}"$group" --height "$height" --width "$width" --seed "$seed" --save_passes '' --write_passes '_img,_id' --save_meshes --num_multiplier "$multiplier" --readout_data_mode"
+    cmd="python3 "$controller_dir"/tdw_physics/target_controllers/"$scenario".py @$arg_name""commandline_args.txt --dir "$output_dir"/"$scenario"${arg_name#.}"$group" --height "$height" --width "$width" --seed "$seed" --save_passes '' --write_passes '_img,_id' --save_meshes --num_multiplier "$multiplier" --readout_data_mode"
     echo $cmd
     eval " $cmd"
 done
